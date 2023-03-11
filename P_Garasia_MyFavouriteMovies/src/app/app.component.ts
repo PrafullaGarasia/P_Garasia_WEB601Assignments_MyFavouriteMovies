1fa39482a00f34ec60fea47a67962792785143fb
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2023-03-11 15:31:15 -0500

    commit

--- P_Garasia_MyFavouriteMovies/src/app/app.component.ts
+++ P_Garasia_MyFavouriteMovies/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MovieService } from './services/movie.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'P_Garasia_MyFavouriteMovies';
+  topMovieContent: any = {};
+  defaultMovieImage: string = 'https://www.pngitem.com/pimgs/m/464-4644133_play-movie-video-clapper-scene-transparent-background-movies.png';
+
+  constructor(private MovieService: MovieService){}
+
+  loggingIDnTitle(content: any){
+    console.log(`ID: ${content.id}`);
+    console.log(`Title: ${content.title}`);
+  }
+
+  ngOnInit(){
+    this.MovieService.getMovieById(4).subscribe(content => this.topMovieContent = content);
+  }
 }
--- P_Garasia_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ P_Garasia_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -1,6 +1,6 @@
-import { keyframes } from "@angular/animations";
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-content-list',
@@ -8,7 +8,7 @@ import { Content } from '../helper-files/content-interface';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
-  contents: Content[];
+  contents: Content[] = [];
   defaultMovieImage: string = 'https://www.pngitem.com/pimgs/m/464-4644133_play-movie-video-clapper-scene-transparent-background-movies.png';
   title: string = '';
   isTitleExists: boolean | null = null;
@@ -22,71 +22,9 @@ searchForTitle(){
   this.isTitleExists = this.contents.some(content => content.title === this.title);
 }
 
-constructor(){
-  this.contents= [
-
-  {
-    id: 0,
-    title: "Black Widow",
-    description: "Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
-    creator: "Cate Shortland",
-    imgURL: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-    type: "Action"    
-  },
-  {
-    id: 1,
-    title: "Doctor Strange in the Multiverse of Madness",
-    description: "Doctor Strange teams up with a mysterious teenage girl from his dreams who can travel across multiverses, to battle multiple threats, including other-universe versions of himself, which threaten to wipe out millions across the multiverse. They seek help from Wanda the Scarlet Witch, Wong and others.",
-    creator: "Sam Raimi",
-    imgURL: "https://m.media-amazon.com/images/M/MV5BNWM0ZGJlMzMtZmYwMi00NzI3LTgzMzMtNjMzNjliNDRmZmFlXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_.jpg"   
-  },
-  {
-    id: 2,
-    title: "Shang-Chi and the Legend of the Ten Rings",
-    description: "Shang-Chi, the master of weaponry-based Kung Fu, is forced to confront his past after being drawn into the Ten Rings organization.",
-    creator: "Destin Daniel Cretton",
-    type: "Fantasy"
-  },
-  {
-    id: 3,
-    title: "Spider-Man: Far from Home",
-    description: "Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.",
-    creator: "Jon Watts",
-    imgURL: "https://cdn.marvel.com/content/1x/spider-manfarfromhome_lob_crd_04_3.jpg",
-    type: "Adventure"    
-  },
-  {
-    id: 4,
-    title: "Thor: Love and Thunder",
-    description: "Thor enlists the help of Valkyrie, Korg and ex-girlfriend Jane Foster to fight Gorr the God Butcher, who intends to make the gods extinct.",
-    creator: "Taika Waititi",
-    imgURL: "https://m.media-amazon.com/images/M/MV5BYmMxZWRiMTgtZjM0Ny00NDQxLWIxYWQtZDdlNDNkOTEzYTdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-    type: "Adventure",
-    tags: ["Taika Waititi", "Adventure"] 
-  },
-  {
-    id: 5,
-    title: "Avengers: Endgame",
-    description: "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
-    creator: "Anthony , RussoJoe Russo",
-    imgURL: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    type: "Action"    
-  },
-  {
-    id: 6,
-    title: "Black Panther",
-    description: "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
-    creator: "Ryan Coogler",
-    type: "Action"    
-  },
-  {
-    id: 7,
-    title: "Captain Marvel",
-    description: "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
-    creator: "Anna Boden, Ryan Fleck",
-    imgURL: "https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_FMjpg_UX1000_.jpg",
-    type: "Adventure"    
-  }
-  ];
+constructor(private MovieService: MovieService){}
+ngOnInit(){
+  this.MovieService.getMovies().subscribe(movies => this.contents = movies);
 }
+  
 }
