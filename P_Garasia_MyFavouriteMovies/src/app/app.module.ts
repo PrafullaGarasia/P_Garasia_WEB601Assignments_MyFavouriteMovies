5d90a55dc31b21384d0bbe934c0d257e0f33751c
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2023-03-19 18:17:32 -0400

    commit

--- P_Garasia_MyFavouriteMovies/src/app/app.module.ts
+++ P_Garasia_MyFavouriteMovies/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContentListComponent } from './content-list/content-list.component';
 import { TypeFilterPipe } from './type-filter.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
 import { CreateContentComponent } from './create-content/create-content.component';
+import { ModifyContentComponentComponent } from './modify-content-component/modify-content-component.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CreateContentComponent } from './create-content/create-content.componen
     ContentListComponent,
     TypeFilterPipe,
     HoverAffectDirective,
-    CreateContentComponent
+    CreateContentComponent,
+    ModifyContentComponentComponent
   ],
   imports: [
     BrowserModule,
